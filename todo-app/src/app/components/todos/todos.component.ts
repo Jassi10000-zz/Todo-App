17871736f6a2a5868a8fdcf8eb69507d7343b84a
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-05-07 23:56:30 +0530

    added sample todos in todo array

--- todo-app/src/app/components/todos/todos.component.ts
+++ todo-app/src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todos} from './../../Models/Todos';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  // so here we an empty array of Todos in todo
+  todo: Todos[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.todo = [
+      {
+        content: "This is the first todo",
+        completed: false
+      },
+      {
+        content: "This is the second todo",
+        completed: true
+      }
+    ]
   }
 
 }
